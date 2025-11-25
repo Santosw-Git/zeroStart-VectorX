11a4e9150208ea40d4c6e008ef177283ba2a01bd
Santosh Khadka <santoshkhadka2326@gmail.com>
2025-11-25 15:03:20 +0545

    gelu activation and feedfoeward code

---
+++ feedForward/feedfor.py
@@ -0,0 +1,22 @@
+import torch    
+import torch.nn as nn
+from layerNormalization.geluactivation import GELU
+from config import GPT_CONFIG_124M
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+print(GPT_CONFIG_124M["emb_dim"])
+
+ffn = FeedForward(GPT_CONFIG_124M)
+x = torch.rand(2, 3, 768) 
+out = ffn(x)
+print(out.shape)
